dee277ea08d7a4e4a75e4fb9fe9ad676a6de98ed
DKSilverX <limit.tith@outlook.com>
2020-04-12 21:56:35 +0700

    Initial sample project

---
+++ src/main/java/io/methea/metheahost/controller/WebClientController.java
@@ -0,0 +1,21 @@
+/**
+ * Author : DKSilverX
+ * Date : 12/04/2020
+ */
+package io.methea.metheahost.controller;
+
+import io.methea.util.SystemUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class WebClientController {
+    @RequestMapping(value = {"/"})
+    public String home(Model model, HttpServletRequest request){
+        model.addAttribute("contextPath", SystemUtils.getBaseUrl(request));
+        return "web/web-host";
+    }
+}
---
+++ src/test/java/io/methea/metheahost/MetheaHostApplicationTests.java
@@ -0,0 +1,13 @@
+package io.methea.metheahost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetheaHostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
